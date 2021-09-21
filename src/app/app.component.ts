b96cf90ba07843b9ab2a721be6e5b6b42d83fa59
sabyav84 <syavasca@gmail.com>
2021-09-21 11:52:05 -0400

    css added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>App Component</h1>'
+  selector: 'mw-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {}
